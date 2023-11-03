f2f9da94ef272926cfdc64358bc0deaa3888a0e5
wodahS21 <yaelelyolo@gmail.com>
2023-11-03 12:18:50 -0600

    Creacion y modificacion del proyecto013

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import  { ArticulosService } from './articulos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto013';
+export class AppComponent implements OnInit{
+
+  articulos: any;
+
+  constructor(private articulosService: ArticulosService) {}
+
+  ngOnInit() {
+    this.articulosService.retornar()
+      .subscribe( result =>  this.articulos = result)
+  }
 }
